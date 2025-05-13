1397a6c2386a09ea08733732cfde53ee7a99ea33
snowfluke <awalariansyah7@gmail.com>
2025-05-13 23:32:33 +0700

    cleanup: remove commented-out code for improved clarity in publish.ts

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,5 +3,4 @@ import { cpToLib, exec } from "./utils";
 // Write required files
 await Promise.all(["./README.md", "./package.json"].map(cpToLib));
 
-// await exec`rm pdf.worker.min.mjs mupdf-wasm.wasm`;
 await exec`cd lib && bun publish --access=public`;
